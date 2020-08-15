9d702849521d123f12eee21715f41b7b4f0f167c
Christie Yoon <cleex1999@gmail.com>
2020-08-15 01:59:51 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,15 +3,6 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
-
-class Dancer
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
